c77c48fe64bd1ec1cc0a064730c62b7f4644edbd
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-11 09:30:24 +0600

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person {
+    String firstName;
+    String lastName;
+    String dateOfBirth;
+    public Person(String firstName, String lastName, String dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                '}';
+    }
+}
